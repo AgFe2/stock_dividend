bd4ae9aa39abebc200a4764c962ed32c430d0c80
You <duug@naver.com>
2022-09-17 18:11:45 +0900

    API 인터페이스 구현

--- src/main/java/com/example/stock_dividend/StockDividendApplication.java
+++ src/main/java/com/example/stock_dividend/StockDividendApplication.java
@@ -14,35 +14,35 @@ import java.io.IOException;
 public class StockDividendApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(StockDividendApplication.class, args);
+        SpringApplication.run(StockDividendApplication.class, args);
 
-        try {
-            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1662940800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
-            Document document = connection.get();
-
-            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element ele = eles.get(0); // table 전체
-
-            Element tbody = ele.children().get(1);
-            for (Element e : tbody.children()) {
-                String txt = e.text();
-                if (!txt.endsWith("Dividend")) {
-                    continue;
-                }
-
-                String[] splits = txt.split(" ");
-                String month = splits[0];
-                int day = Integer.valueOf(splits[1].replace(",", ""));
-                int year = Integer.valueOf(splits[2]);
-                String dividend = splits[3];
-
-                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1662940800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+//            Document document = connection.get();
+//
+//            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+//            Element ele = eles.get(0); // table 전체
+//
+//            Element tbody = ele.children().get(1);
+//            for (Element e : tbody.children()) {
+//                String txt = e.text();
+//                if (!txt.endsWith("Dividend")) {
+//                    continue;
+//                }
+//
+//                String[] splits = txt.split(" ");
+//                String month = splits[0];
+//                int day = Integer.valueOf(splits[1].replace(",", ""));
+//                int year = Integer.valueOf(splits[2]);
+//                String dividend = splits[3];
+//
+//                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+//
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
