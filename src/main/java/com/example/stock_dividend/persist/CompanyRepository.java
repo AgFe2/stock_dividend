3abd251638f9f988af4300be31b513ace67b3e1b
You <duug@naver.com>
2022-09-17 18:42:28 +0900

    엔티티 구현

---
+++ src/main/java/com/example/stock_dividend/persist/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.example.stock_dividend.persist;
+
+
+import com.example.stock_dividend.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+}
