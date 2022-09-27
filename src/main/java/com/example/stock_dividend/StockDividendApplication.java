127ab1260a1127ec3e9003f03040aca0191b458a
You <duug@naver.com>
2022-09-27 11:12:18 +0900

    Scraping Scheduler 기능 구현 및 로그 기능 구현

--- src/main/java/com/example/stock_dividend/StockDividendApplication.java
+++ src/main/java/com/example/stock_dividend/StockDividendApplication.java
@@ -2,8 +2,10 @@ package com.example.stock_dividend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockDividendApplication {
 
     public static void main(String[] args) {
