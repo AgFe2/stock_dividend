f4f4aa400de032a614035b73ffdcd1d30cf92fa7
You <duug@naver.com>
2022-09-29 16:49:30 +0900

    회원가입, 로그인 controller 구현

--- src/main/java/com/example/stock_dividend/web/AuthController.java
+++ src/main/java/com/example/stock_dividend/web/AuthController.java
@@ -1,4 +1,40 @@
 package com.example.stock_dividend.web;
 
+import com.example.stock_dividend.model.Auth;
+import com.example.stock_dividend.model.MemberEntity;
+import com.example.stock_dividend.security.TokenProvider;
+import com.example.stock_dividend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/singup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+        // 회원가입을 위한 API
+        MemberEntity result = this.memberService.register(request);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/singin")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignIn request) {
+        // 로그인을 위한 API
+        MemberEntity member = this.memberService.authenticate(request);
+        String token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
 }
